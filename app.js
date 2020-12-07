fd1ec9dff93e35bb8d085df92e369b3da5b70f33
pchandu <praneethchandu@gmail.com>
2020-12-06 17:03:17 -0800

    barebones HTML and intro routes

--- app.js
+++ app.js
@@ -3,39 +3,40 @@ const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
 const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
-
+const token = ""; // how do we do this again? something similar to what's above I guess?
 app.use(express.static('public'))
 
 app.get('/', (request, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-// create route to get single book by its isbn
-app.get('/books/:isbn', (request, response) => {
-  // make api call using fetch
-  fetch(`http://openlibrary.org/api/books?bibkeys=ISBN:${request.params.isbn}&format=json&jscmd=data`)
-  .then((response) => {
-      return response.text();
-  }).then((body) => {
-      let results = JSON.parse(body)
-      console.log(results)   // logs to server
-      response.send(results) // sends to frontend
-    });
-});
-
-// create a search route
-app.get('/search', (request, response) => {
-  fetch(`http://openlibrary.org/search.json?q=${request.query.string}`)
-  .then((response) => {
-      return response.text();
-  }).then((body) => {
-      let results = JSON.parse(body)
-      console.log(results)
-      response.send(results)
-    });
-});
-
 app.listen(PORT, () => {
   console.log(__dirname);
   console.log(`listening on ${PORT}`)
 })
+
+//getting a stock price target
+// /stock/{symbol}/price-target
+app.get(`/stock/${symbol}/price-target`, (request, response) => {
+    fetch(`https://cloud.iexapis.com/stable/stock/${request.body}/price-target?token=${token}`)
+        .then((response) => {
+            console.log(response)
+            return response;
+        });
+});
+
+// example route
+// // create route to get single book by its isbn
+// app.get('/books/:isbn', (request, response) => {
+//   // make api call using fetch
+//   fetch(`http://openlibrary.org/api/books?bibkeys=ISBN:${request.params.isbn}&format=json&jscmd=data`)
+//   .then((response) => {
+//       return response.text();
+//   }).then((body) => {
+//       let results = JSON.parse(body)
+//       console.log(results)   // logs to server
+//       response.send(results) // sends to frontend
+//     });
+// });
+
+var axios = require("axios").default;
