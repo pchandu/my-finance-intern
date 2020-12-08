fc2162cc8d8e05a08384ab07f3cd1229d7569c14
pchandu <praneethchandu@gmail.com>
2020-12-07 21:01:16 -0800

    basic app infra

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const fetch = require('node-fetch')
-const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
-const token = ""; // how do we do this again? something similar to what's above I guess?
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+global.fetch = require("node-fetch");
+const token = process.env.IEX; 
+const input = "TSLA";
 app.use(express.static('public'))
 
-app.get('/', (request, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
 
-app.listen(PORT, () => {
-  console.log(__dirname);
-  console.log(`listening on ${PORT}`)
-})
+console.log("testing out the IEX call from this file");
+fetch(`https://cloud.iexapis.com/stable/stock/${input}/quote?token=${token}`)
+        .then((response) => response.json())
+        .then(data => console.log(data));
+//DOM event listener -- 
 
 //getting a stock price target
 // /stock/{symbol}/price-target
 app.get(`/stock/${symbol}/price-target`, (request, response) => {
-    fetch(`https://cloud.iexapis.com/stable/stock/${request.body}/price-target?token=${token}`)
-        .then((response) => {
-            console.log(response)
-            return response;
-        });
+    fetch(`https://cloud.iexapis.com/stable/stock/${input}/price-target?token=${token}`)
+        .then((response) => response.json())
+        .then(data => console.log(data));
 });
 
-// example route
-// // create route to get single book by its isbn
-// app.get('/books/:isbn', (request, response) => {
-//   // make api call using fetch
-//   fetch(`http://openlibrary.org/api/books?bibkeys=ISBN:${request.params.isbn}&format=json&jscmd=data`)
-//   .then((response) => {
-//       return response.text();
-//   }).then((body) => {
-//       let results = JSON.parse(body)
-//       console.log(results)   // logs to server
-//       response.send(results) // sends to frontend
-//     });
-// });
 
-var axios = require("axios").default;
+
+
+// const path = require('path')
+// const PORT = process.env.PORT || 8000; 
+
+// app.get('/', (request, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'))
+// })
+
+// app.listen(PORT, () => {
+//     console.log(__dirname);
+//     console.log(`listening on ${PORT}`)
+// })
