859eca1c92c7f392572604e93d7fb2903e8059c9
pchandu <praneethchandu@gmail.com>
2020-12-07 21:32:15 -0800

    preliminary input testing

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const token = process.env.IEX;
 const input = "TSLA";
 app.use(express.static('public'))
 
+document.addEventListener('keypress', e => {
+    if(e.key === 'Enter'){
+        //check if the input is not blank
+        //pass the input into the first API call to get the stock quote data
+         
+    }
+})
 
 console.log("testing out the IEX call from this file");
 fetch(`https://cloud.iexapis.com/stable/stock/${input}/quote?token=${token}`)
@@ -15,14 +22,16 @@ fetch(`https://cloud.iexapis.com/stable/stock/${input}/quote?token=${token}`)
 
 //getting a stock price target
 // /stock/{symbol}/price-target
-app.get(`/stock/${symbol}/price-target`, (request, response) => {
-    fetch(`https://cloud.iexapis.com/stable/stock/${input}/price-target?token=${token}`)
-        .then((response) => response.json())
-        .then(data => console.log(data));
-});
 
 
 
+
+
+// app.get(`/stock/${symbol}/price-target`, (request, response) => {
+//     fetch(`https://cloud.iexapis.com/stable/stock/${input}/price-target?token=${token}`)
+//         .then((response) => response.json())
+//         .then(data => console.log(data));
+// });
 
 // const path = require('path')
 // const PORT = process.env.PORT || 8000; 
